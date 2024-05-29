85d27aa2da628d1480a7565df603f2112aa743bc
Alejandro183368 <183368@upslp.edu.mx>
2024-05-28 20:49:49 -0600

    blog_update

--- js/script.js
+++ js/script.js
@@ -1,6 +1,43 @@
+document.getElementById('post-form').addEventListener('submit', function(e) {
+    e.preventDefault();
+
+    // Obtener valores del formulario
+    const title = document.getElementById('title').value;
+    const content = document.getElementById('content').value;
+    const user = document.getElementById('User').value;
+
+
+    // Crear un nuevo elemento de publicación
+    const post = document.createElement('div');
+    post.classList.add('post');
+
+    // Agregar título y contenido a la publicación
+    const postTitle = document.createElement('h2');
+    postTitle.textContent = title;
+    const postContent = document.createElement('h3');
+    postContent.textContent = content;
+    const postUser = document.createElement('p');
+    postUser.textContent = user;
+    const hrElement = document.createElement('hr');
+    hrElement.classList.add('my-4');
+
+    post.appendChild(postTitle);
+    post.appendChild(postContent);
+    post.appendChild(postUser);
+    post.appendChild(hrElement);
+    // Agregar la nueva publicación al blog
+    document.getElementById('blog-posts').appendChild(post);
+
+    // Limpiar el formulario
+    document.getElementById('post-form').reset();
+});
+
+
 function redirigir(url) {
     window.location.href = url ; 
 }
+
+
 function PDF() {
     const { jsPDF } = window.jspdf;
     const doc = new jsPDF();
